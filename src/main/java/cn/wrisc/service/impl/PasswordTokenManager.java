15048ab5a5bc247b877bb10267b5d636ff5580fa
Neo Chen <neoxen@qq.com>
2015-08-11 11:23:19 +0800

    project init

---
+++ src/main/java/cn/wrisc/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package cn.wrisc.service.impl;
+
+import cn.wrisc.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
---
+++ src/test/java/cn/wrisc/dao/GenericDaoTest.java
@@ -0,0 +1,31 @@
+package cn.wrisc.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import cn.wrisc.dao.hibernate.GenericDaoHibernate;
+import cn.wrisc.model.User;
+import org.hibernate.SessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class GenericDaoTest extends BaseDaoTestCase {
+    Log log = LogFactory.getLog(GenericDaoTest.class);
+    GenericDao<User, Long> genericDao;
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Before
+    public void setUp() {
+        genericDao = new GenericDaoHibernate<>(User.class, sessionFactory);
+    }
+
+    @Test
+    public void getUser() {
+        User user = genericDao.get(-1L);
+        assertNotNull(user);
+        assertEquals("user", user.getUsername());
+    }
+}
